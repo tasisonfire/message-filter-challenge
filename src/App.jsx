3c238ec6ddcf2bd3d729b1da113d6f4df1aa178c
phanuwit13 <big.phanuwit@gmail.com>
2022-07-02 12:19:31 +0700

    init project

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,19 @@
 import './App.css'
-import { useState, useEffect } from 'react'
-
-//components
-import Header from './components/header'
-import UserList from './components/userList'
-
-//icon
+import { useState } from 'react'
 
 //mock data
 import userData from './userData'
 
 function App() {
   const [showData, setShowData] = useState(userData)
-  const [keyword, setKeyword] = useState('')
-
-  const onSetKeyword = (e) => {
-    setKeyword(e.target.value)
-  }
-
-  useEffect(() => {
-    let newData = userData.filter((item) => {
-      return item.first_name
-        .concat(` ${item.last_name}`)
-        .toLowerCase()
-        .includes(keyword.toLocaleLowerCase())
-    })
-    setShowData(newData)
-  }, [keyword])
 
   return (
     <div className='container'>
-      <Header keyword={keyword} setKeyword={onSetKeyword} />
-      <UserList showData={showData} />
+      <ul>
+        {showData.map((user) => {
+          return <li key={user.id}>{`${user.first_name} ${user.last_name}`}</li>
+        })}
+      </ul>
     </div>
   )
 }
